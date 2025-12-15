da3cecc7909bad82f51e104da0490049305d800d
Hanif Musa Daut <hanifmusadaud@gmail.com>
2025-12-16 05:16:53 +0700

    Menambahkan Program baru boss

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
+kata = input("Masukan Kata: ")
+print(f"Kata dari anda: {kata}, Terimakasih atas kata-kata anda!")
 
+nomor = input("Masukan Nomor anda: ")
+print("Berdasarkan data yang anda masukan, nomor anda adalah: ",nomor)
