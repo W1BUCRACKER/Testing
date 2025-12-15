123347755388a9409eea2c663197c6c341113f42
Hanif Musa Daut <hanifmusadaud@gmail.com>
2025-12-16 05:02:14 +0700

    Merubah variable dan menambahkan Kode

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
-input = input("Masukan Kata: ")
-print(f"Kata dari anda: {input}, Terimakasih atas kata-kata anda!")
-print("Terimaksaih pak jokowi")
+kata = input("Masukan Kata: ")
+print(f"Kata dari anda: {kata}, Terimakasih atas kata-kata anda!")
+
+umur = int(input("Berapa umurmu: "))
+print("Berdasarkan data yang anda masukan, umur ana adalah: ", umur)
+
+nomor = input("Masukan Nomor anda: ")
+print("Berdasarkan data yang anda masukan, nomor anda adalah: ",nomor)
