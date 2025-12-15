f34c036b9da108245e3dd94f412fa2085c871abb
Hanif Musa Daut <hanifmusadaud@gmail.com>
2025-12-16 04:41:27 +0700

    Update dan penambahan kode

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 input = input("Masukan Kata: ")
 print(f"Kata dari anda: {input}, Terimakasih atas kata-kata anda!")
-print("Terimaksaih pak jokowi")
+
+input2 = input("Berapa umurmu: ")
+print("Berdasarkan data yang anda masukan, umur ana adalah: ", input2)
